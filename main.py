33318e0ba2f9579e59ec011e0c42c978da228159
Ed Witkowski <EdWit1971@gmail.com>
2022-06-12 18:24:58 -0400

    Finally got the Text Label to Scroll Inside the Scroll Window

--- main.py
+++ main.py
@@ -8,14 +8,17 @@
 
 from kivymd.app import MDApp
 
-from kivymd.uix.label import MDLabel
+from kivymd.uix.label       import MDLabel
 from kivymd.uix.floatlayout import MDFloatLayout
+
+from kivy.clock import Clock
 
 from kivy.core.window import Window
 
-from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.floatlayout    import FloatLayout
+from kivy.effects.scroll import ScrollEffect
+
 from kivy.uix.scrollview     import ScrollView
+from kivy.uix.relativelayout import RelativeLayout
 
 from Misc_DrawStuff import DrawStuff
 
@@ -30,113 +33,154 @@ class ScrollViewApp(MDApp):
         super(ScrollViewApp, self).__init__(**kwargs)
         ##########################
         self.Win_To_Draw = MDFloatLayout()
-        self.Label1      = MDLabel()
+        self.LText       = MDLabel()
         self.Draw_Lines1 = DrawStuff()
-        self.Draw_Lines2 = DrawStuff()
-        self.ScrViewWin  = ScrollView()
-        self.Scrolling   = RelativeLayout()
+        self.ScrView  = ScrollView()      #RecycleView
+        self.ScrLayout   = RelativeLayout()  #RecycleLayout
         ##########################
+        self.Event_Timer = Clock.create_trigger(callback = self.Callback_Timer, \
+                                                timeout = 3.0, \
+                                                interval = False)
         return
 
     def build(self):
         ScrollViewApp.title = 'ScrollViewApp'
         #############################################
         self.Win_To_Draw.size = Window.size
-        #############################################
         self.Draw_Lines1.Show_Instructions(self.Win_To_Draw)
-        self.Draw_Lines2.Show_Instructions(self.Scrolling)
-        self.Draw_Lines2.Clear_IG()
         #############################################
         Screen_Width  = self.Win_To_Draw.width
         Screen_Height = self.Win_To_Draw.height
         Xc = int(Screen_Width * 0.5)
         Yc = int(Screen_Height * 0.5)
-        Screen_Xo = Xc - int(Screen_Width * 0.5)
-        Screen_Yo = Yc - int(Screen_Height * 0.5)
-        Screen_Xf = Screen_Xo + Screen_Width
-        Screen_Yf = Screen_Yo + Screen_Height
+        # Screen_Xo = Xc - int(Screen_Width * 0.5)
+        # Screen_Yo = Yc - int(Screen_Height * 0.5)
+        # Screen_Xf = Screen_Xo + Screen_Width
+        # Screen_Yf = Screen_Yo + Screen_Height
         #############################################
         ###########  ScrollView #####################
         #############################################
-        self.Scr_Width  = int(Screen_Width * 0.3333)
-        self.Scr_Height = int(Screen_Height * 0.75)
+        Scr_Width  = int(Screen_Width * 0.333)
+        Scr_Height = int(Screen_Height * 0.5)
         #################
-        self.Scr_Xo   = Xc - int(self.Scr_Width * 0.5)
-        self.Scr_Xf   = self.Scr_Xo + self.Scr_Width
+        Scr_Xo = Xc - int(Scr_Width * 0.5)
+        Scr_Xf = Scr_Xo + Scr_Width
         #################
-        self.Scr_Yo = Yc - int(self.Scr_Height * 0.5)
-        self.Scr_Yf = self.Scr_Yo + self.Scr_Height
+        Scr_Yo = Yc - int(Scr_Height * 0.5)
+        Scr_Yf = Scr_Yo + Scr_Height
         #############################################
-        self.Draw_Lines1.Draw_Fill_Rectangle(pXo = self.Scr_Xo, \
-                                             pXf = self.Scr_Xf, \
-                                             pYo = self.Scr_Yo, \
-                                             pYf = self.Scr_Yf, \
+        self.Draw_Lines1.Draw_Fill_Rectangle(pXo = Scr_Xo, \
+                                             pXf = Scr_Xf, \
+                                             pYo = Scr_Yo, \
+                                             pYf = Scr_Yf, \
                                              pR = 0.8, \
                                              pG = 0.8, \
                                              pB = 0.8, \
                                              pW = 2)
-        self.Draw_Lines1.Draw_Rectangle(pXo = self.Scr_Xo, \
-                                        pXf = self.Scr_Xf, \
-                                        pYo = self.Scr_Yo, \
-                                        pYf = self.Scr_Yf, \
+        self.Draw_Lines1.Draw_Rectangle(pXo = Scr_Xo, \
+                                        pXf = Scr_Xf, \
+                                        pYo = Scr_Yo, \
+                                        pYf = Scr_Yf, \
                                         pR = 0, \
                                         pG = 0, \
                                         pB = 0, \
                                         pW = 3)
         #############################################
+        self.LText.font_style = 'Subtitle2'
+        self.LText.font_size  = 14
+        self.LText.size_hint  = None, None
+        self.LText.width      = Scr_Width
+        self.LText.height     = Scr_Height
+        self.LText.text_size  = (self.LText.width, None)
+        self.LText.x          = 0
+        self.LText.y          = 0
+        self.LText.halign     = 'left'
+        self.LText.valign     = 'bottom'
+        self.LText.padding_y  = 5
+        self.LText.theme_text_color = 'Custom'
+        self.LText.color      = (0, 0, 0, 1)
+        self.LText.text  = '  Some Random Things...'
+        self.LText.text += '\n  PeanutButter and Jelly Sandwich'
+        self.LText.text += '\n  Cuban Cigar'
+        self.LText.text += '\n  Fred Flintstone'
+        self.LText.text += '\n  Underwater Lost City'
+        self.LText.text += '\n  Bang Zoom to the Moon'
+        self.LText.text += '\n  Red White and Blue'
+        self.LText.text += '\n  Duck Billed Platypus'
+        self.LText.text += '\n  Green Eyed Monster'
+        self.LText.text += '\n  Isosceles Triangle'
+        self.LText.text += '\n  Tuba and Trumbone'
+        self.LText.text += '\n  Hey Hey Ralphie Boy'
+        self.LText.text += '\n  Buy One Get One Free'
+        self.LText.text += '\n  Where No Man Has Gone'
+        self.LText.text += '\n  Hole in One'
+        self.LText.text += '\n  Here Today Gone Tomorrow'
+        self.LText.text += '\n  Loose Lips Sink Ships'
+        self.LText.text += '\n  Hairy Potato'
+        self.LText.text += '\n  PeanutButter and Jelly Sandwich'
+        self.LText.text += '\n  Cuban Cigar'
+        self.LText.text += '\n  Fred Flintstone'
+        self.LText.text += '\n  Underwater Lost City'
+        self.LText.text += '\n  Bang Zoom to the Moon'
+        self.LText.text += '\n  Red White and Blue'
+        self.LText.text += '\n  Duck Billed Platypus'
+        self.LText.text += '\n  Green Eyed Monster'
+        self.LText.text += '\n  Isosceles Triangle'
+        self.LText.text += '\n  Tuba and Trumbone'
+        self.LText.text += '\n  Hey Hey Ralphie Boy'
+        self.LText.text += '\n  Buy One Get One Free'
+        self.LText.text += '\n  Where No Man Has Gone'
+        self.LText.text += '\n  Hole in One'
+        self.LText.text += '\n  Here Today Gone Tomorrow'
+        self.LText.text += '\n  Loose Lips Sink Ships'
+        self.LText.text += '\n  Hairy Potato'
+        #############################################
         ###########  ScrollView #####################
         #############################################
-        self.Scrolling.width     = self.Scr_Width
-        self.Scrolling.height    = self.Scr_Height * 2.0
-        self.Scrolling.size_hint = (1, None)
-        #self.Scrolling.size_hint = (1, 1)
-        self.ScrViewWin.do_scroll_x = False
-        self.ScrViewWin.do_scroll_y = True
-        self.ScrViewWin.scroll_type = ['content']
-        self.ScrViewWin.width       = self.Scr_Width
-        self.ScrViewWin.height      = self.Scr_Height
-        self.ScrViewWin.x           = self.Scr_Xo
-        self.ScrViewWin.y           = self.Scr_Yo
+        self.ScrView.size_hint   = None, None
+        self.ScrView.size        = (Scr_Width, Scr_Height)
+        self.ScrView.width       = Scr_Width
+        self.ScrView.height      = Scr_Height
+        self.ScrView.x           = Scr_Xo
+        self.ScrView.y           = Scr_Yo
+        self.ScrView.do_scroll_x = False
+        self.ScrView.do_scroll_y = True
+        self.ScrView.effect_cls  = ScrollEffect
+        self.ScrView.scroll_type = ['content']
         #############################################
-        y_bottom = 5
-        y_top = self.ScrViewWin.height - 5
-        y_incr = int((y_top - y_bottom) / 4)
-        y = y_bottom
-        for i in range(5):
-            # Relative Coordinates
-            self.Draw_Lines2.Draw_Line(pX1 = 10, \
-                                       pY1 = y, \
-                                       pX2 = self.Scr_Width - 10, \
-                                       pY2 = y, \
-                                       pR  = 0.4, \
-                                       pG  = 0, \
-                                       pB  = 0, \
-                                       pW  = 4)
-            y = y + y_incr
+        self.ScrLayout.size_hint_y = None
+        self.ScrLayout.width  = Scr_Width
+        #self.ScrLayout.height = Scr_Height
+        self.ScrLayout.x      = 0 #Scr_Xo
+        self.ScrLayout.y      = 0 #Scr_Yo
         #############################################
-        self.Label1.font_style = 'Subtitle2'
-        self.Label1.font_size  = 18
-        self.Label1.size_hint  = (None, None)
-        self.Label1.width      = int(self.Win_To_Draw.width / 4)
-        self.Label1.height     = int(self.Win_To_Draw.height / 30)
-        self.Label1.text_size  = (self.Label1.width, self.Label1.height)
-        self.Label1.x          = 0
-        self.Label1.y          = 0
-        self.Label1.halign     = 'left'
-        self.Label1.valign     = 'bottom'
-        self.Label1.color      = (0, 0, 0, 1)
-        self.Label1.text       = 'Testing...'
-        if(self.Label1.parent == None):
-            self.Scrolling.add_widget(self.Label1)
+        
+        if(self.ScrView.parent == None):
+            self.Win_To_Draw.add_widget(self.ScrView)
+        
+        if(self.ScrLayout.parent == None):
+            self.ScrView.add_widget(self.ScrLayout)
+        
+        if(self.LText.parent == None):
+            self.ScrLayout.add_widget(self.LText)
+        
         #############################################
-        if(self.Scrolling.parent == None):
-            self.ScrViewWin.add_widget(self.Scrolling)
-        if(self.ScrViewWin.parent == None):
-            self.Win_To_Draw.add_widget(self.ScrViewWin)
-        #############################################
-        self.ScrViewWin.scroll_y = 1  # 0 Bottom touches bottom or 1
-        #############################################
+        self.Event_Timer()
         return self.Win_To_Draw
+    
+    
+    #################################################
+    def Callback_Timer(self, dt):
+        width  = self.LText.texture_size[0]
+        height = self.LText.texture_size[1]
+        self.LText.size_hint = (None, None)
+        self.LText.width     = width
+        self.LText.height    = height
+        self.LText.text_size = (width, None)
+        self.ScrLayout.height = height + 15
+        self.ScrView.scroll_y = 0  # 0 Bottom touches bottom or 1
+        return
+    
 
 ScrollViewApp().run()
 
